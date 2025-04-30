0bcac29b322e9339e92893515f7da61a50698a4b
maksim-volkmann <auksinistonic@gmail.com>
2025-04-30 21:24:12 +0200

    ex02 starting pojt

---
+++ ex02/Array.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <exception>
+
+template <typename T>
+class Array{
+public:
+	Array();
+	// Creates an array of n elements initialized by default.
+	// Tip: Try to compile int * a = new int(); then display *a.
+	Array(unsigned int);
+	Array(const Array&);
+	Array& operator=(const Array&);
+	~Array();
+
+	T& operator[](unsigned int);
+	const T& operator[](unsgined int) const;
+	unsigned int size() const;
+
+
+
+};	

2abf2d14d06f0213838ab18e9b63464a77332b4a
maksim-volkmann <auksinistonic@gmail.com>
2025-05-16 21:08:03 +0200

    ex01. modified

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,7 +2,7 @@
 
 template <typename T>
 void iter(T* arr, int length, void (*f)(T&)){
-	if(!arr || !f)
+	if(!arr || !f || length <= 0)
 		return;
 	for(int i = 0; i < length; ++i){
 		f(arr[i]);
@@ -11,7 +11,7 @@ void iter(T* arr, int length, void (*f)(T&)){
 
 template <typename T>
 void iter(const T* arr, int length, void (*f)(const T&)){
-	if(!arr || !f)
+	if(!arr || !f || length <= 0)
 		return;
 	for(int i = 0; i < length; ++i){
 		f(arr[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,33 +1,35 @@
 #include "iter.hpp"
 #include <iostream>
+#include <string>
 
 class Person {
 private:
-	std::string name_;
-	int age_;
+	std::string _name;
+	int _age;
 public:
-	Person(const std::string name, int age) : name_(name), age_(age){}
-
-
-	void sayMyName() const {
-		std::cout << "Hello, my name is" << name_ << std::endl;
-	}
+	Person(const std::string name, int age) : _name(name), _age(age){}
 
 	void makeMeOlder(){
-		std::cout << "Age before: " << age_ << std::endl;
-		age_++;
-		std::cout << "Age after: " << age_ << std::endl;
+		printInfo();
+		_age++;
+		printInfo();
+		std::cout << "-----------------" << std::endl;
+	}
+
+	void printInfo() const {
+		std::cout << "Hello, my name is " << _name <<
+			" and current age is " << _age << "." << std::endl;
 	}
 };
 
-
-
-void increment(int &a){
-	a = a + 1;
+template <typename T>
+void increment(T& num) {
+	num += 1;
 }
 
-void printStuff(const int &b){
-	std::cout << b << std::endl;
+template <typename T>
+void printStuff(const T& num) {
+	std::cout << num << std::endl;
 }
 
 //for class
@@ -35,28 +37,45 @@ void age(Person &p){
 	p.makeMeOlder();
 }
 
+void printPersonInfo(const Person& p) {
+	p.printInfo();
+}
+
+
+
 int main(){
-	Person people[] = {Person("Rick", 25), Person("Morty", 30)};
-
 	int a[] = {1, 2, 3};
-	const int b[] = {10, 11, 12};
+	const int b[] = {10, 11, 12, 42};
+	int* c = 0;
+	double d[] = {4.4, 5.5, 6.6};
+	Person people[] = {Person("Rick", 25), Person("Morty", 30), Person("Summer", 130)};
 
-
+	std::cout << "Before incrementation: " << std::endl;
+	iter(a, 3, printStuff);
+	std::cout << "-----------------" << std::endl;
 	iter(a, 3, increment);
+	std::cout << "After incrementation: " << std::endl;
+	iter(a, 3, printStuff);
+	std::cout << "-----------------" << std::endl;
 
-	for (int i = 0; i < 3; ++i){
-		std::cout << a[i] << " ";
-	}
+	iter(b, 4, printStuff);
+	std::cout << "-----------------" << std::endl;
 
-	std::cout << "\n-----------------" << std::endl;
+	std::cout << "EMPTY STRING: " << std::endl;
+	iter(c, 0, printStuff);
+	std::cout << "-----------------" << std::endl;
 
-	iter(b, 3, printStuff);
+	std::cout << "Before incrementation: " << std::endl;
+	iter(d, 3, printStuff);
+	iter(d, 3, increment);
+	std::cout << "After incrementation: " << std::endl;
+	iter(d, 3, printStuff);
+	std::cout << "-----------------" << std::endl;
 
-	std::cout << "\n-----------------" << std::endl;
+	std::cout << "Class: \n" << std::endl;
+	iter(people, 3, age);
 
-
-
-	iter(people, 2, age);
+	iter(people, 3, printPersonInfo);
 
 	return 0;
 }

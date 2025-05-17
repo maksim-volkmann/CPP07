84c6eb941baf5ce7ca789af4eba43f9892b059a8
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2025-05-17 17:21:18 +0200

    ex02 done:

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <exception>
+#include <stdexcept>
 
 template <typename T>
 class Array{
 public:
 	Array();
-	// Creates an array of n elements initialized by default.
-	// Tip: Try to compile int * a = new int(); then display *a.
 	Array(unsigned int);
 	Array(const Array&);
 	Array& operator=(const Array&);
@@ -20,5 +18,56 @@ public:
 private:
 	T* _data;
 	unsigned int _size;
+};
 
-};	
+template <typename T>
+Array<T>::Array() : _data(NULL), _size(0) {}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _data(new T[n]()), _size(n) {}
+
+// todo: test if copies correctly.
+template <typename T>
+Array<T>::Array(const Array& other) : _data(NULL), _size(0) {
+	*this = other;
+}
+
+// arr1 = arr2;
+template <typename T>
+Array<T>& Array<T>::operator=(const Array& other){
+	if(this != &other) {
+		delete[] _data;
+		_size = other._size;
+		_data = new T[_size];
+		for(unsigned int i = 0; i < _size; i++) {
+			_data[i] = other._data[i];
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+Array<T>::~Array() {
+	delete[] _data;
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int index){
+	if(index >= _size){
+		throw std::out_of_range("INDEX IS OUT OF BOUNDS!");
+	}
+	return _data[index];
+}
+
+template <typename T>
+const T& Array<T>::operator[](unsigned int index) const{
+	if(index >= _size){
+		throw std::out_of_range("INDEX IS OUT OF BOUNDS!");
+	}
+	return _data[index];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const{
+	return _size;
+}
